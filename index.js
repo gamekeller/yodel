5165d2c606c3d6fe01d727ce63207b77eb97059e
Heinrich Fenkart <hnrch02@gmail.com>
2015-07-05 16:05:01 +0200

    Use SSH tunnel for TeamSpeak server query connection

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 import redis from 'redis'
-import config from './lib/config'
+import tunnel from 'tunnel-ssh'
 import teamspeak from './lib/client'
 import Monitor from './lib/monitor'
 import rpc from './lib/rpc'
 
-let redisClient = redis.createClient(config.redis.port, config.redis.host)
-  .on('connect', () => console.log('✔ Redis connection established.'))
-  .on('error', () => console.error('✗ Unable to connect to Redis.'))
+let config = require('./lib/config')('YODEL')
 
-teamspeak.connect(config.teamspeak)
-  .on('connect', () => console.log('✔ TeamSpeak Server Query connection established.'))
-  .on('error', () => console.error('✗ Unable to connect to the TeamSpeak Server Query.'))
+function init (err) {
+  if (err) return console.error('✗ Unable to establish SSH tunnel.', err)
+  if (config.tunnel.active) console.log('✔ SSH tunnel established.')
 
-rpc.listen(config.port)
-console.log(`✔ RPC server listening on port ${ config.port }.`)
+  let redisClient = redis.createClient(config.redis.port, config.redis.host)
+    .on('connect', () => console.log('✔ Redis connection established.'))
+    .on('error', () => console.error('✗ Unable to connect to Redis.'))
 
-let monitor = new Monitor(teamspeak, redisClient)
+  teamspeak.connect(config.teamspeak)
+    .on('connect', () => console.log('✔ TeamSpeak Server Query connection established.'))
+    .on('error', () => console.error('✗ Unable to connect to the TeamSpeak Server Query.'))
+
+  rpc.listen(config.port)
+  console.log(`✔ RPC server listening on port ${ config.port }.`)
+
+  new Monitor(teamspeak, redisClient)
+}
+
+if (config.tunnel.active) {
+  tunnel({
+    host: config.tunnel.host,
+    dstPort: config.tunnel.port,
+    username: config.tunnel.user,
+    password: config.tunnel.pass
+  }, init)
+} else {
+  init()
+}
