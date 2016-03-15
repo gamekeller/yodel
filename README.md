87ac74509ec1614df02c5184ca47500de5da880a
Heinrich Fenkart <hnrch02@gmail.com>
2016-03-15 14:53:44 +0100

    Rip out `tunnel-ssh`, recommend `autossh` solution for tunneling

--- README.md
+++ README.md
@@ -2,6 +2,51 @@
 [![dependency Status](https://david-dm.org/gamekeller/yodel/status.svg)](https://david-dm.org/gamekeller/yodel#info=dependencies)
 [![devDependency Status](https://david-dm.org/gamekeller/yodel/dev-status.svg)](https://david-dm.org/gamekeller/yodel#info=devDependencies)
 
-TeamSpeak bridge and future bot for [gamekeller.net](https://gamekeller.net)
+TeamSpeak bot for [gamekeller.net](https://gamekeller.net)
 
 Released under the terms of the [MIT license](LICENSE).
+
+
+## Tunnel
+
+We strongly recommend using a SSH tunnel to forward the TeamSpeak Query connection if it is hosted on a different server than yodel.
+Otherwise all commands (including login credentials) would be transmitted in plain text over the network, exposing them to potential MITM attcks.
+
+Here's an example of how to set up [`autossh`](http://www.harding.motd.ca/autossh/) for this purpose:
+
+```
+# Be root
+sudo -i
+
+# Install autossh
+apt-get install autossh
+
+# Generate keys (make sure to leave the passphrase empty)
+ssh-keygen -t rsa -b 4096 -C "yodel"
+
+# Add public key as authorized key on the target server
+
+# Set up SSH config
+cat << EOF >> ~/.ssh/config
+Host teamspeak-tunnel
+  HostName             example.com
+  User                 yodel
+  IdentityFile         ~/.ssh/id_rsa-yodel
+  LocalForward         10011 localhost:10011
+  ServerAliveInterval  5
+  ServerAliveCountMax  2
+  ExitOnForwardFailure yes
+EOF
+
+# Try it out
+autossh -M 0 teamspeak-tunnel
+
+# Add startup script
+touch /etc/network/if-up.d/teamspeak-tunnel
+chmod +x /etc/network/if-up.d/teamspeak-tunnel
+
+cat << EOF > /etc/network/if-up.d/teamspeak-tunnel
+#!/bin/sh
+sudo /usr/bin/autossh -M 0 -fNT teamspeak-tunnel &
+EOF
+```
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import pmx from 'pmx'
 import fs from 'fs'
 import path from 'path'
 import Redis from 'ioredis'
-import tunnel from 'tunnel-ssh'
 import teamspeak from './lib/client'
 import rpc from './lib/rpc'
 import cfg from './lib/config'
@@ -11,37 +10,21 @@ pmx.init()
 
 let config = cfg('YODEL')
 
-function init (err) {
-  if (err) return console.error('✗ Unable to establish SSH tunnel.', err)
-  if (config.tunnel.active) console.log('✔ SSH tunnel established.')
+let redis = new Redis(config.redis.port, config.redis.host, { keyPrefix: 'YDL:' })
+  .on('connect', () => console.log('✔ Redis connection established.'))
+  .on('error', () => console.error('✗ Unable to connect to Redis.'))
 
-  let redis = new Redis(config.redis.port, config.redis.host, { keyPrefix: 'YDL:' })
-    .on('connect', () => console.log('✔ Redis connection established.'))
-    .on('error', () => console.error('✗ Unable to connect to Redis.'))
+teamspeak.connect(config.teamspeak)
+  .on('connect', () => console.log('✔ TeamSpeak Server Query connection established.'))
+  .on('error', () => console.error('✗ Unable to connect to the TeamSpeak Server Query.'))
 
-  teamspeak.connect(config.teamspeak)
-    .on('connect', () => console.log('✔ TeamSpeak Server Query connection established.'))
-    .on('error', () => console.error('✗ Unable to connect to the TeamSpeak Server Query.'))
+rpc.listen(config.port)
+console.log(`✔ RPC server listening on port ${ config.port }.`)
 
-  rpc.listen(config.port)
-  console.log(`✔ RPC server listening on port ${ config.port }.`)
-
-  fs.readdir(path.join(__dirname, 'modules'), (err, files) => {
-    for (let file of files) {
-      let YodelModule = require(path.join(__dirname, 'modules', file)).default
-      let moduleConfig = config.modules[path.basename(file, '.js')]
-      new YodelModule(teamspeak, redis, moduleConfig)
-    }
-  })
-}
-
-if (config.tunnel.active) {
-  tunnel({
-    host: config.tunnel.host,
-    dstPort: config.tunnel.port,
-    username: config.tunnel.user,
-    password: config.tunnel.pass
-  }, init)
-} else {
-  init()
-}
+fs.readdir(path.join(__dirname, 'modules'), (err, files) => {
+  for (let file of files) {
+    let YodelModule = require(path.join(__dirname, 'modules', file)).default
+    let moduleConfig = config.modules[path.basename(file, '.js')]
+    new YodelModule(teamspeak, redis, moduleConfig)
+  }
+})
