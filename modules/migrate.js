70ff7205aac0200f934b26068dfce968b2897362
Heinrich Fenkart <hnrch02@gmail.com>
2016-04-20 20:49:31 +0200

    Add migrate module

---
+++ modules/migrate.js
@@ -0,0 +1,36 @@
+import _ from 'lodash'
+import Promise from 'bluebird'
+import Debug from '../lib/util'
+import YodelModule from '../lib/module'
+
+let debug = new Debug('yodel:migrate')
+
+export default class Migrate extends YodelModule {
+  constructor (teamspeak, redis) {
+    super(teamspeak, redis)
+
+    this.teamspeak
+      .on('connect', ::this.onConnect)
+      .on('client.enter', ::this.onClientEnter)
+  }
+
+  handleCluid (cluid) {
+    this.redis.hget('migrate', cluid).then((result) => {
+      if (!result) return
+
+      this.teamspeak.addToServerGroupByCluid(cluid, result.split(',')).then(() => {
+        this.redis.hdel('migrate', cluid)
+      })
+    })
+  }
+
+  onConnect (onlineClients) {
+    _.each(onlineClients, (client) => {
+      this.handleCluid(client.client_unique_identifier)
+    })
+  }
+
+  onClientEnter (client) {
+    this.handleCluid(client.client_unique_identifier)
+  }
+}
