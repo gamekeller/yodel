c202a2e1b1d8b00b0d28ca54c2f2f5b6897bd3fd
Heinrich Fenkart <hnrch02@gmail.com>
2018-05-06 12:02:37 +0200

    Client: Handle connection failures better
    - Time between connection attempts increased
    - Limit attempts to 100 (to force a restart by the process manager)

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ let redis = new Redis(config.redis.port, config.redis.host, { keyPrefix: 'YDL:',
 
 teamspeak.connect(config.teamspeak)
   .on('connect', () => console.log('✔ TeamSpeak Server Query connection established.'))
-  .on('error', () => console.error('✗ Unable to connect to the TeamSpeak Server Query.'))
+  .on('error', (err) => {
+    if (err) console.error('✗ TeamSpeak Server Query connection error:\n', err)
+    else console.error('✗ Unable to connect to the TeamSpeak Server Query.')
+  })
 
 fs.readdir(path.join(__dirname, 'modules'), (err, files) => {
   for (let file of files) {
